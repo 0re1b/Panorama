b95a11784cf65e3c7ad5ecaa63b7b71d3e82e714
0re1b <orelbitan123@gmail.com>
2016-09-18 15:40:30 +0300

    you're welcome bro

--- src/setup.py
+++ src/setup.py
@@ -4,7 +4,7 @@ from Tkinter import *
  
 setup(
 	name = 'Panorama',
-	pulisher='Almog Cohen',
+	publisher='Almog Cohen',
 	description = 'Fast incident overview',
 	version = '1.0',
 	options = {'py2exe':{'bundle_files': 1, "includes":["Tkinter"], "dll_excludes": ["msvcr90.dll","msvcp90.dll","msvcm90.dll","tcl85.dll","tk85.dll","mfc90.dll","mfc90u.dll","mfcm90.dll","mfcm90u.dll"]}},
